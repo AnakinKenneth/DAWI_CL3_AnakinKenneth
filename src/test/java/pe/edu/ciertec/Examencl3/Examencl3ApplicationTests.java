3804b25b4d6a20ae4e14a5b5dc0aade82ec6a8c2
Luis181996 <luishumberto181996.com>
2023-11-26 10:15:09 -0500

    primer commit

---
+++ src/test/java/pe/edu/ciertec/Examencl3/Examencl3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.ciertec.Examencl3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examencl3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
